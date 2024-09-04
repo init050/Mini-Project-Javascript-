8fd3b17f482ff6044aa471b8d89a56020fdd776f
init050 <9hoh3n1ex@protonmail.com>
2024-09-04 15:52:41 +0330

    config project 2

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import './output.css'
 
 
-document.querySelector('#app').innerHTML = '
-'
+document.querySelector("#app").innerHTML = `
+  <div class="p-5">
+    <h1>تسک های امروز</h1>
+    
+  </div>
+`;
 
 
-
+console.log('wwwwwwwwww')
 
 setupCounter(document.querySelector('#counter'))
