31fa1709e05d0134a4114d371fb5c46a49ff2548
tibaasgari <tibaasgari@gmail.com>
2024-09-07 22:48:45 +0430

    creating and reading have  been done

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
-import './output.css'
 
-
-let tasks = []
+let storage =localStorage.getItem("tasks")
+let tasks = storage ? JSON.parse(storage) :[]
 
 document.querySelector("#app").innerHTML = `
   <h1 class="text-3xl"> تسک های امروز</h1>
@@ -12,9 +11,11 @@ document.querySelector("#app").innerHTML = `
     <textarea id="task-dec" placeholder='توضیحات' class="border p-2 rounded-md w-full mb-2"></textarea>
     <button id="add-task-submit" class="bg-blue-500 text-white px-4 py-2 rounded-md">اضافه کردن تسک</button>
   </div>
+  <div id ="task-list"></div>
 
 
 `;
+
 
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -24,8 +25,11 @@ document.addEventListener("DOMContentLoaded", function () {
   
   const inputTaskTitle = document.getElementById('task-title');
   const inputTaskDec = document.getElementById('task-dec');
-
   const addTaskSubmit = document.getElementById("add-task-submit");
+  const taskList = document.querySelector("#task-list");
+  const editBtn =document.querySelector("#edit")
+
+  
 
 
   // Show form when adding task button is clicked. 
@@ -41,11 +45,13 @@ document.addEventListener("DOMContentLoaded", function () {
     // Reset form after adding task.
     taskform.classList.add('hidden');
     addTaskBtn.classList.remove('hidden');
+    
 
 
     const taskTitle = inputTaskTitle.value;
     const taskDec = inputTaskDec.value;
     console.log(taskTitle, taskDec)
+    if(!taskTitle || !taskDec) throw new Error ("task cant be empty")
 
 
     const task = {
@@ -63,17 +69,85 @@ document.addEventListener("DOMContentLoaded", function () {
 
     tasks.unshift(task)
     console.log(tasks)
+    render();
+  });
     
     // render(); //TODO: It should be called after adding the Function render => UPDATE UI.
+    function render() {
+      taskList.innerHTML=tasks.map((task)=>
+        `<div  class="m-5 p-7 h-30 flex justify-between items-center border-r-red-700 border-r-4  rounded-xl border border-gray-200 ">
+          <div class="flex gap-5">
+            <div class="form-control self-start">
+              <label class="label cursor-pointer">
+                <input type="checkbox" checked="checked" class="checkbox checkbox-primary border border-gray-400" />
+              </label>
+            </div>
+            <div >
+              <h3 class="font-bold mb-3">${task.title}</h3>
+              <p class="text-gray-500">${task.dec}</p>
+            </div>
+          </div>
+          <div  class=" flex flex-col  cursor-pointer gap-2" >
+            <div>
+              <img class="setting" src="./assets/Frame 1000005552.png" alt="setting">
+            </div>
+            <div class="settingList" data-id = ${task.id} class="flex gap-2 ">
+              <button class="btn btn-sm delete">delete</button>
+              <button  class=" btn btn-sm ">edit</button>
+            </div>
+          </div>
+      
+         </div>`).join("")
+         localStorage.setItem("tasks",JSON.stringify(tasks))
+      
+    }
+    render();
+    
+   /* set.addEventListener("click", () =>{
+      
+      settingList.innerHTML =` 
+      <div class="flex border p-2 rounded-sm">
+        <img src="./tabler_trash-x.png" alt="delete">
+        <img src="./tabler_edit.png" alt="edit">
+      </div>
+      `
+    });
+    */
+  
+    
+    taskList.addEventListener("click",(event)=>{
+      if(event.target.classList.contains("delete")){
+        const taskId = parseInt(event.target.parentNode.dataset.id)
+        tasks=tasks.filter((task) => task.id !== taskId)
+        
+      }
+ 
+      
+       if(event.target.classList.contains("edit")){
+        const taskId = parseInt(event.target.parentNode.dataset.id)
+        tasks=tasks.map((task) =>
+           task.id === taskId ?{...task,title:"newtitle"}:task)
+        
+      }
+      /* if(event.target.classList.contains("setting")){
+        settingList.innerHTML =` 
+        <div class="flex border p-2 rounded-sm">
+          <img src="./tabler_trash-x.png" alt="delete">
+          <img src="./tabler_edit.png" alt="edit">
+        </div>
+        `
+        
+       } 
+        */
+       render(); 
 
-  });
+
+    })
+   /* editBtn.addEventListener("click",()=>{
+      
+      
+    })
+    */
+  
 
 });
-
-
-
-
-
-
-
-
